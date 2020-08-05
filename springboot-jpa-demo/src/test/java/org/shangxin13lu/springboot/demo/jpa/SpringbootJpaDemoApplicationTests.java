093e5b7f293a9bae913d708958ce71519d59f81f
shangxin13lu <1258337703@qq.com>
2020-08-05 22:54:11 +0800

    maven parent

---
+++ springboot-jpa-demo/src/test/java/org/shangxin13lu/springboot/demo/jpa/SpringbootJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shangxin13lu.springboot.demo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot-mybatis-demo/src/test/java/org/shangxin13lu/springboot/demo/mybatis/SpringbootMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shangxin13lu.springboot.demo.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
